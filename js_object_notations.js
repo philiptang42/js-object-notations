9a4c35ff84dd65b3aa2cd190e1361225d86c47d9
philiptang42 <philiptang42@gmail.com>
2016-02-15 22:51:28 -0500

    notation names

--- js_object_notations.js
+++ js_object_notations.js
@@ -1,5 +1,7 @@
 // help us make snoopy using literal notation
 // Remember snoopy is a "beagle" and is 10 years old.
+
+//Object Literal Notation
 var snoopy = {
   species: "beagle",
   age: 10
@@ -8,6 +10,8 @@ var snoopy = {
 
 // help make buddy using constructor notation
 // buddy is a "golden retriever" and is 5 years old
+
+//Object Constructor Notation
 var buddy = new Object();
 buddy.species = "golden retriever";
 buddy.age = 5;
