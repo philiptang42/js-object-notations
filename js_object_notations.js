5d8217bd2468b225dcae7a36e2a6de7f46e7ac3d
philiptang42 <philiptang42@gmail.com>
2016-02-18 07:51:43 -0500

    using 'this' keyword

--- js_object_notations.js
+++ js_object_notations.js
@@ -35,3 +35,16 @@ bob.getYearOfBirth = function () {
   return 2014 - bob.age;
 };
 console.log(bob.getYearOfBirth());
+
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+// and down here we just use the method we already made
+bob.setAge = setAge;
+
+// change bob's age to 50 here
+bob.setAge(50);
