9dc01bba7d5869a1bec245015c4d303b530a6875
philiptang42 <philiptang42@gmail.com>
2016-02-21 22:48:53 -0500

    custom object constructor

--- js_object_notations.js
+++ js_object_notations.js
@@ -86,3 +86,15 @@ square.calcArea = function() {
 
 var p = square.calcPerimeter();
 var a = square.calcArea();
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+
+var george = new Person("George Washington", 275);
