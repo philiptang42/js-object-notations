221bbfd0b08df36a95a51df5786f32b30a8fb405
philiptang42 <philiptang42@gmail.com>
2016-02-21 23:43:36 -0500

    sally and holden objects

--- js_object_notations.js
+++ js_object_notations.js
@@ -110,3 +110,14 @@ function Dog(breed, size, bark_volume) {
   this.size = size;
   this.bark_volume = bark_volume;
 };
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.species = "Homo Sapiens";
+}
+
+var sally = new Person("Sally Bowles", 39)
+var holden = new Person("Holden Caulfield", 16)
+console.log("sally's species is " + sally.species + " and she is " + sally.age + " years old." );
+console.log("holden's species is " + holden.species + " and he is " + holden.age + " years old.");
