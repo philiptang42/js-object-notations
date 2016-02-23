6745a556f52ccaaa9cfa5a96e02925ab77403d06
philiptang42 <philiptang42@gmail.com>
2016-02-22 23:20:35 -0500

    rabbit object constructors

--- js_object_notations.js
+++ js_object_notations.js
@@ -139,3 +139,18 @@ var area = rex.calcArea();
 var perimeter = rex.calcPerimeter();
 
 console.log(perimeter)
+
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+var rabbit1 = new Rabbit("fluffy");
+rabbit1.describeMyself;
+var rabbit2 = new Rabbit("happy");
+rabbit2.describeMyself;
+var rabbit3 = new Rabbit("sleepy");
+rabbit3.describeMyself;
